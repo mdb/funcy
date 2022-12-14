a446572dbfcbafa6def3e02ff6cb19d09f5e6dce
Mike Ball <mikedball@gmail.com>
2022-12-14 09:05:47 -0500

    run gofmt -w

--- funcy.go
+++ funcy.go
@@ -1,64 +1,64 @@
 package funcy
 
 import (
-  "golang.org/x/exp/constraints"
-  "sort"
+	"golang.org/x/exp/constraints"
+	"sort"
 )
 
-func Contains[E comparable] (s []E, v E) bool {
-  for _, vs := range s {
-    if v == vs {
-      return true
-    }
-  }
+func Contains[E comparable](s []E, v E) bool {
+	for _, vs := range s {
+		if v == vs {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
 func Reverse[E any](s []E) []E {
-  result := make([]E, 0, len(s))
+	result := make([]E, 0, len(s))
 
-  for i := len(s) - 1; i >= 0; i-- {
-    result = append(result, s[i])
-  }
+	for i := len(s) - 1; i >= 0; i-- {
+		result = append(result, s[i])
+	}
 
-  return result
+	return result
 }
 
 func Dedupe[E comparable](s []E) []E {
-  result := make([]E, 0, len(s))
+	result := make([]E, 0, len(s))
 
-  for _, item := range s {
-    if !Contains(result, item) {
-      result = append(result, item)
-    }
-  }
+	for _, item := range s {
+		if !Contains(result, item) {
+			result = append(result, item)
+		}
+	}
 
-  return result
+	return result
 }
 
 func Sort[E constraints.Ordered](s []E) []E {
-  result := make([]E, len(s))
+	result := make([]E, len(s))
 
-  copy(result, s)
+	copy(result, s)
 
-  sort.Slice(result, func(i, j int) bool {
-    return result[i] < result[j]
-  })
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
+	})
 
-  return result
+	return result
 }
 
 type mapFunc[E any] func(E) E
 
 func Map[E any](s []E, f mapFunc[E]) []E {
-  result := make([]E, len(s))
+	result := make([]E, len(s))
 
-  for i := range s {
-    result[i] = f(s[i])
-  }
+	for i := range s {
+		result[i] = f(s[i])
+	}
 
-  return result
+	return result
 }
 
 type keepFunc[E any] func(E) bool
@@ -80,9 +80,9 @@ type reduceFunc[E any] func(E, E) E
 func Reduce[E any](s []E, init E, f reduceFunc[E]) E {
 	cur := init
 
-  for _, v := range s {
-  	cur = f(cur, v)
-  }
+	for _, v := range s {
+		cur = f(cur, v)
+	}
 
-  return cur
+	return cur
 }
