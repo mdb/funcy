998f3a8b74985df2354319ec9449f948da8245ca
Mike Ball <mikedball@gmail.com>
2023-01-17 07:25:44 -0500

    add executable examples

--- funcy_test.go
+++ funcy_test.go
@@ -1,9 +1,18 @@
 package funcy
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
+
+func ExampleContains() {
+	list := []string{"foo", "bar", "baz"}
+
+	fmt.Println(Contains(list, "foo"))
+	//
+	// Output: true
+}
 
 func TestContains(t *testing.T) {
 	list := []string{"foo", "bar", "baz"}
@@ -21,6 +30,14 @@ func TestContains(t *testing.T) {
 	}
 }
 
+func ExampleDedupe() {
+	list := []string{"foo", "foo", "bar"}
+
+	fmt.Println(Dedupe(list))
+	//
+	// Output: [foo bar]
+}
+
 func TestDedupe(t *testing.T) {
 	list := []string{"foo", "foo", "bar"}
 
@@ -29,6 +46,14 @@ func TestDedupe(t *testing.T) {
 	if len(got) != len(expected) || got[0] != expected[0] || got[1] != expected[1] {
 		t.Fatalf("expected: %v, got: %v", expected, got)
 	}
+}
+
+func ExampleReverse() {
+	list := []string{"foo", "bar", "baz"}
+
+	fmt.Println(Reverse(list))
+	//
+	// Output: [baz bar foo]
 }
 
 func TestReverse(t *testing.T) {
@@ -41,6 +66,14 @@ func TestReverse(t *testing.T) {
 	}
 }
 
+func ExampleSort() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Sort(list))
+	//
+	// Output: [1 2 3]
+}
+
 func TestSort(t *testing.T) {
 	list := []int{3, 1, 2}
 
@@ -49,6 +82,16 @@ func TestSort(t *testing.T) {
 	if expected[0] != got[0] && expected[2] != got[2] {
 		t.Fatalf("expected: %v, got: %v", expected, got)
 	}
+}
+
+func ExampleMap() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Map(list, func(item int) int {
+		return item + 2
+	}))
+	//
+	// Output: [5 3 4]
 }
 
 func TestMap(t *testing.T) {
@@ -63,6 +106,16 @@ func TestMap(t *testing.T) {
 	}
 }
 
+func ExampleFilter() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Filter(list, func(item int) bool {
+		return item == 3
+	}))
+	//
+	// Output: [3]
+}
+
 func TestFilter(t *testing.T) {
 	list := []int{3, 1, 2}
 
@@ -73,6 +126,16 @@ func TestFilter(t *testing.T) {
 	if expected[0] != got[0] && len(expected) != len(got) {
 		t.Fatalf("expected: %v, got: %v", expected, got)
 	}
+}
+
+func ExampleFind() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Find(list, func(item int) bool {
+		return item%3 == 0
+	}))
+	//
+	// Output: 3 <nil>
 }
 
 func TestFind(t *testing.T) {
@@ -98,6 +161,16 @@ func TestFind(t *testing.T) {
 	}
 }
 
+func ExampleAny() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Any(list, func(item int) bool {
+		return item%3 == 0
+	}))
+	//
+	// Output: true
+}
+
 func TestAny(t *testing.T) {
 	list := []int{3, 1, 2}
 
@@ -120,6 +193,16 @@ func TestAny(t *testing.T) {
 	}
 }
 
+func ExampleReduce() {
+	list := []int{3, 1, 2}
+
+	fmt.Println(Reduce(list, 0, func(cur, val int) int {
+		return cur + val
+	}))
+	//
+	// Output: 6
+}
+
 func TestReduce(t *testing.T) {
 	list := []int{3, 1, 2}
 
@@ -140,22 +223,33 @@ func TestReduce(t *testing.T) {
 	}
 }
 
+func ExampleReduceRight() {
+	list := []int{2, 5, 10}
+
+	fmt.Println(ReduceRight(list, 20, func(cur, val int) int {
+		return cur - val
+	}))
+	//
+	// Output: 3
+}
+
 func TestReduceRight(t *testing.T) {
-	list := []int{3, 1, 2}
+	list := []int{2, 5, 10}
 
-	got := ReduceRight(list, 0, func(cur, val int) int {
-		return cur + val
+	got := ReduceRight(list, 20, func(cur, val int) int {
+		return cur - val
 	})
-	expected := 6
+	expected := 3
 	if expected != got {
 		t.Fatalf("expected: %v, got: %v", expected, got)
 	}
 
-	got = Reduce(list, 2, func(cur, val int) int {
-		return cur + val
+	got = ReduceRight(list, 10, func(cur, val int) int {
+		return cur - val
 	})
-	expected = 8
+	expected = -7
 	if expected != got {
 		t.Fatalf("expected: %v, got: %v", expected, got)
 	}
+
 }
